9f9c1e3deb3be360eaf8b1c7ed88339bb761c5fb
Daniel <Brisenodan12@gmail.com>
2026-02-04 23:21:51 -0600

    initial setup, have the loop reading input through source, pipe is directed to filter input stream > test1.txt, ISSUE: NOSUCHELEMENT EXCEPTION WHEN EOF, ALSO NEED TO FIX GROUPS COUNTER INCREMENT LOGIC

--- Filter.java
+++ Filter.java
@@ -11,28 +11,34 @@ import java.io.IOException;
 import java.io.FileNotFoundException;
 
 /**
-   This program filters.
-
-   When playing with file filters, it is useful to know that "end-of-file" is
-   denoted at the command-line in Windows by Ctrl-Z and by Ctrl-D in Linux.
-*/
-public class Filter
-{
-   public static void main(String[] args)
-   {
+ * This program filters.
+ * 
+ * When playing with file filters, it is useful to know that "end-of-file" is
+ * denoted at the command-line in Windows by Ctrl-Z and by Ctrl-D in Linux.
+ */
+public class Filter {
+   public static void main(String[] args) {
       final Scanner in = new Scanner(System.in);
-
-      int columns = 3;    // Default number of columns.
+      int columns = 3; // Default number of columns.
       int precision = 13; // Default number of digits after a decimal point.
-      int groups = 0;     // Default size of a group. Zero means no groups.
-
-      while(in.hasNextDouble())
-      {
-         double input = in.nextDouble();
-         System.out.println(input);
+      int groups = 0; // Default size of a group. Zero means no groups.
+      int groupsCounter = 0;
+      int width = 4 + 1 + precision;
+      String fmt = "%" + width + "." + precision + "f";
+      while (in.hasNextDouble()) {
+         int i;
+         for (i = 0; i < columns; i++) {
+            Double input = in.nextDouble();
+            System.out.printf(fmt, input);
+            System.out.print(" ");
+         }
+         if (groupsCounter == groups) {
+            System.out.println("");
+            groupsCounter = 0;
+            i = 0;
+         }
+         groupsCounter++;
       }
-
-
 
    }
 }
