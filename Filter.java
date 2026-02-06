5b2b03c6b2ac412bf26e90ea07f8d9a6d45d8a4a
Wuizlz <brisenodan12@gmail.com>
2026-02-06 09:04:19 -0600

    filtering format works, now need to get precedence of filter vars

--- Filter.java
+++ Filter.java
@@ -19,26 +19,31 @@ import java.io.FileNotFoundException;
 public class Filter {
    public static void main(String[] args) {
       final Scanner in = new Scanner(System.in);
-      int columns = 3; // Default number of columns.
-      int precision = 13; // Default number of digits after a decimal point.
-      int groups = 0; // Default size of a group. Zero means no groups.
-      int groupsCounter = 0;
+      int columns = 7; // Default number of columns.
+      int precision = 6; // Default number of digits after a decimal point.
+      int groups = 12; // Default size of a group. Zero means no groups.
+      int groupsCounter = 1;
+      int colCounter = 1;
       int width = 4 + 1 + precision;
       String fmt = "%" + width + "." + precision + "f";
       while (in.hasNextDouble()) {
-         int i;
-         for (i = 0; i < columns; i++) {
-            Double input = in.nextDouble();
+         if (colCounter <= columns && (groupsCounter <= groups || groups == 0)) {
+            colCounter++;
+            groupsCounter++;
+            double input = in.nextDouble();
             System.out.printf(fmt, input);
             System.out.print(" ");
+         } else if (colCounter > columns) {
+            System.out.println();
+            colCounter = 1;
+         } else if (groupsCounter > groups) {
+            System.out.println("\n");
+            groupsCounter = 1;
+            colCounter = 1;
          }
-         if (groupsCounter == groups) {
-            System.out.println("");
-            groupsCounter = 0;
-            i = 0;
-         }
-         groupsCounter++;
-      }
 
+      }
+      in.close();
    }
+
 }
